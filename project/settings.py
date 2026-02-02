b4fb8c8d319640d376db4882cb2f7dc93e362bbb
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-02-02 18:35:44 +0600

    Add custom user model configuration and include user app in installed apps

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.user',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'project.wsgi.application'
+AUTH_USER_MODEL = "user.CustomUser"
 
 
 # Database
