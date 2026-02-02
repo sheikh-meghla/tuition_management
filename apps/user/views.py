0ed306ca82a826132ff5ae0dd7ff9887696bcaf9
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-02-02 19:59:58 +0600

    Add subject app with initial setup and user migration

--- apps/user/views.py
+++ apps/user/views.py
@@ -2,7 +2,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, OutstandingToken
 
 from .serializers import SignUpSerializer, SignInSerializer, SignOutSerializer
 
