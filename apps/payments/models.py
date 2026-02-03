fea6afcef7b9edd810731911728a52a10721a29d
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-02-03 15:41:19 +0600

    create payments apps/ models.py

--- apps/payments/models.py
+++ apps/payments/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class FeePayment(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    month = models.CharField(max_length=20)
+
+    bkash_number = models.CharField(max_length=15)
+    otp = models.CharField(max_length=6, blank=True, null=True)
+
+    STATUS = [
+        ('PENDING', 'Pending'),
+        ('PAID', 'Paid'),
+        ('FAILED', 'Failed'),
+    ]
+    status = models.CharField(max_length=10, choices=STATUS, default='PENDING')
+
+    transaction_id = models.CharField(max_length=100, blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
