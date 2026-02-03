4349e3cc8e7c9f679be933630ed9fc05c27cca2f
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-02-03 15:43:22 +0600

    create payments apps/ admin.py

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from unfold.admin import ModelAdmin
+from .models import FeePayment
 # Register your models here.
+@admin.register(FeePayment)
+class FeePaymentAdmin(ModelAdmin):
+    model = FeePayment
+    list_display = ("student", "amount", "month", "status",)
+    list_filter = ("student", "month", "status",)
+    search_fields = ("student__email", "transaction_id",)
+    fieldsets = (
+        (None, {"fields": ("student", "amount", "month", "status", "transaction_id",)}),
+    )
