ae10f82f840ca0866c603cf14967ceb064d5c65f
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-02-03 15:25:37 +0600

    project apps urls.py add

--- project/urls.py
+++ project/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('apps.user.urls')),
     path('api/subject/', include('apps.subject.urls')),
+    path('api/fee/', include('apps.fee.urls')),
 ]
